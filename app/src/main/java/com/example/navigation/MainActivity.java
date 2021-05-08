589d06c3e482a287efdf724b76f4f428325156f6
IJLAL AHMAD <ijlalahmad845@gmail.com>
2021-05-08 23:02:08 +0530

    Initial commit

---
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -0,0 +1,224 @@
+package com.example.navigation;
+
+import androidx.annotation.IntegerRes;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.solver.state.State;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.constraintlayout.widget.ConstraintSet;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.TimeInterpolator;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.icu.text.RelativeDateTimeFormatter;
+import android.os.Bundle;
+import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewAnimationUtils;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.navigation.NavigationView;
+
+import java.io.Serializable;
+import java.net.IDN;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity instance;
+
+    NavigationView nav;
+    ActionBarDrawerToggle toggle;
+    DrawerLayout drawerLayout;
+
+    LinearLayout linearLayout;
+    TextView matrixcount;
+    HorizontalScrollView scrollView;
+    int counter=0;
+
+
+    ArrayList<TextView> textlist=new ArrayList<>();
+    ArrayList<String> stringlist=new ArrayList<>();
+    ArrayList<Integer> IDlist=new ArrayList<>();
+    ArrayList<CardView> cardlist=new ArrayList<>();
+    ArrayList<ConstraintLayout> ClLlist=new ArrayList<>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        instance=this;
+
+        linearLayout=findViewById(R.id.linearLayout);
+        matrixcount=findViewById(R.id.matrixcount);
+        scrollView=findViewById(R.id.scrollview);
+
+
+
+        initdrawer();
+
+
+    }
+
+    public void inittextviews(int index,String s){
+
+            textlist.get(index).setText(s);
+    }
+    public void secondactivity(View v){
+        Intent intent=new Intent(this,MatrixInput.class);
+        startActivity(intent);
+    }
+    public static MainActivity getInstance() {
+        return instance;
+    }
+    public void addcard(View v){
+
+        ConstraintLayout Cl=new ConstraintLayout(this);
+        ImageButton imageButton=new ImageButton(this);
+        imageButton.setId(View.generateViewId());
+        imageButton.setImageResource(R.drawable.ic_trash);
+        imageButton.setBackgroundColor(Color.TRANSPARENT);
+
+        TextView textView=new TextView(this);
+        textView.setText(String.valueOf(counter++));
+        textView.setId(View.generateViewId());
+
+
+
+
+
+
+        //ripple effect
+        TypedValue outValue = new TypedValue();
+        this.getTheme().resolveAttribute(android.R.attr.selectableItemBackgroundBorderless,outValue, true);
+
+        imageButton.setBackgroundResource(outValue.resourceId);
+
+
+        Cl.addView(imageButton);
+        Cl.addView(textView);
+
+        textlist.add(textView);
+        stringlist.add(textView.getText().toString());
+        IDlist.add(textView.getId());
+
+        ConstraintSet set=new ConstraintSet();
+        set.clone(Cl);
+        set.connect(imageButton.getId(),ConstraintSet.RIGHT,ConstraintSet.PARENT_ID,ConstraintSet.RIGHT,16);
+        set.connect(imageButton.getId(),ConstraintSet.TOP,ConstraintSet.PARENT_ID,ConstraintSet.TOP,0);
+
+        set.connect(textView.getId(),ConstraintSet.RIGHT,ConstraintSet.PARENT_ID,ConstraintSet.RIGHT,0);
+        set.connect(textView.getId(),ConstraintSet.LEFT,ConstraintSet.PARENT_ID,ConstraintSet.LEFT,0);
+        set.connect(textView.getId(),ConstraintSet.TOP,imageButton.getId(),ConstraintSet.BOTTOM,0);
+
+     
+
+
+        set.applyTo(Cl);
+
+
+
+
+        CardView cardView = new CardView(this);
+        final float scale = cardView.getContext().getResources().getDisplayMetrics().density;
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                (int) (150 * scale + 0.5f),
+                (int) (200 * scale + 0.5f)
+        );
+        params.setMargins((int) (10 * scale + 0.5f), (int) (20 * scale + 0.5f), (int) (10 * scale + 0.5f), (int) (10 * scale + 0.5f));
+
+
+
+        cardView.setElevation(10f);
+        cardView.setRadius(10f);
+
+
+        cardView.addView(Cl);
+        cardView.setAlpha(0f);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+    @Override
+    public void onClick(View v) {
+        //ArrayList<TextView> object = new ArrayList<>();
+
+        Intent intent = new Intent(getApplicationContext(), MatrixInput.class);
+        intent.putExtra("com.example.navigation.index",IDlist.indexOf(textView.getId()));
+        intent.putExtra("com.example.navigation.String_list",stringlist);
+        startActivity(intent);
+
+
+    }
+});
+        linearLayout.addView(cardView,linearLayout.getChildCount()-1,params);
+        cardView.animate().alpha(1.0f).setDuration(200).setListener(null);
+        scrollView.fullScroll(View.FOCUS_RIGHT);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cardView.animate().alpha(0f).setDuration(200).setListener(new AnimatorListenerAdapter() {
+                    @Override
+                    public void onAnimationEnd(Animator animation) {
+                        linearLayout.removeView(cardView);
+                        matrixcount.setText("Total Matrices : "+(linearLayout.getChildCount()-1));
+
+                        Cl.removeAllViews();
+
+                        super.onAnimationEnd(animation);
+                    }
+                });
+            }
+        });
+        matrixcount.setText("Total Matrices : "+(linearLayout.getChildCount()-1));
+    }
+
+    public void initdrawer(){
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        nav=findViewById(R.id.nav);
+        drawerLayout=findViewById(R.id.drawer);
+
+        toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+
+    }
+
+    @Override
+    public void onBackPressed() {
+        if(drawerLayout.isDrawerOpen(GravityCompat.START))
+            drawerLayout.closeDrawer(GravityCompat.START);
+        else
+        super.onBackPressed();
+    }
+}
---
+++ app/src/main/java/com/example/navigation/MatrixInput.java
@@ -0,0 +1,406 @@
+package com.example.navigation;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.app.Instrumentation;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.inputmethodservice.Keyboard;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+
+import java.util.ArrayList;
+
+public class MatrixInput extends AppCompatActivity {
+
+    public EditText editText;
+    int index;
+    int row=2,col=2,focusX=0,focusY=0;
+    int p=0;
+
+    MaterialButton rowUp,rowDown,colUp,colDown;
+
+    Button[] btns=new Button[11];
+    ImageButton[] imgbtns=new ImageButton[5];
+
+
+    AutoCompleteTextView act;
+    TextInputLayout til;
+    ArrayList<String> Name=new ArrayList<>();
+    ArrayAdapter<String> adt;
+    CardView keyboardcard;
+
+
+
+
+    TextInputEditText[][] matrixFields=new TextInputEditText[5][5];
+    TextInputLayout[][] textInputLayouts=new TextInputLayout[5][5];
+
+
+    TextView rowText,colText;
+
+
+    ArrayList<String> stringArrayList=new ArrayList<>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_matrix_input);
+
+        //matrixField=findViewById(R.id.filledTextField);
+        initializewidgets();
+        keyboardcard=findViewById(R.id.keyboardcard);
+
+        til=findViewById(R.id.menu);
+
+        act=findViewById(R.id.textfield);
+        Name.add("A");Name.add("B");Name.add("C");Name.add("D");
+
+        adt=new ArrayAdapter<>(getApplicationContext(), android.R.layout.simple_list_item_activated_1,Name);
+        act.setAdapter(adt);
+        act.setThreshold(1);
+        act.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                til.setErrorEnabled(false);
+                //til.setBoxStrokeColor(Color.GREEN);
+            }
+        });
+
+
+        ArrayList<String> object = getIntent().getStringArrayListExtra("com.example.navigation.String_list");
+        index=getIntent().getIntExtra("com.example.navigation.index",-1);
+
+        editText.setText(object.get(index));
+
+
+    }
+
+    public void controlmatrixsize(View v){
+        if(rowUp.isPressed() && row<4){
+            rowText.setText(String.valueOf(++row+1));
+
+        }
+
+
+        else if(rowDown.isPressed() && row>0)
+        {
+            rowText.setText(String.valueOf(--row+1));
+
+        }
+
+        if(colUp.isPressed() && col<4)
+        {
+            colText.setText(String.valueOf(++col+1));
+
+        }
+
+        else if(colDown.isPressed() && col>0)
+        {
+            colText.setText(String.valueOf(--col+1));
+
+        }
+
+        matrixFields[focusY][focusX].requestFocus();
+        ShowHideMatrix();
+
+
+    }
+
+    public void ShowHideMatrix(){
+        for(int i=0;i<5;i++)
+            for(int j=0;j<5;j++)
+                textInputLayouts[i][j].setVisibility(View.GONE);
+
+        for(int i=0;i<=row;i++)
+            for(int j=0;j<=col;j++)
+                textInputLayouts[i][j].setVisibility(View.VISIBLE);
+
+
+    }
+
+    public void showhidekeyboard(View v){
+
+        if(p==1){
+            keyboardcard.animate().alpha(1.0f).setDuration(70).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    keyboardcard.setVisibility(View.VISIBLE);
+                    super.onAnimationEnd(animation);
+                }
+            });
+
+        }
+        else if(p==0){
+            keyboardcard.animate().alpha(0.0f).setDuration(150).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    keyboardcard.setVisibility(View.GONE);
+                    super.onAnimationEnd(animation);
+                }
+            });
+
+        }
+        p=1-p;
+
+    }
+
+    public void numpad(View v){
+        String str;
+        int cursorindex;
+
+                for(int i=0;i<5;i++)
+                    for(int j=0;j<5;j++)
+                        if(matrixFields[i][j].isFocused()) {
+                            str = matrixFields[i][j].getText().toString();
+                            cursorindex=matrixFields[i][j].getSelectionStart();
+
+                            for(int k=0;k<10;k++){
+                                if(btns[k].isPressed()){
+                                    matrixFields[i][j].setText(str.substring(0,cursorindex)+k+str.substring(cursorindex));
+                                    matrixFields[i][j].setSelection(cursorindex+1);
+                                }
+                            }
+
+                            if(btns[10].isPressed() && !matrixFields[i][j].getText().toString().contains(".")){
+                                matrixFields[i][j].setText(str.substring(0,cursorindex)+"."+str.substring(cursorindex));
+                                matrixFields[i][j].setSelection(cursorindex+1);
+                            }
+
+                            else if (imgbtns[0].isPressed() && cursorindex>0){
+                                matrixFields[i][j].setText(str.substring(0,cursorindex-1)+str.substring(cursorindex));
+                                matrixFields[i][j].setSelection(cursorindex-1);
+                            }
+                            else if (imgbtns[0].isPressed() && cursorindex==0){
+                                movefocus(v);
+                            }
+
+                        }
+        //Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+    }
+
+    public void movefocus(View v){
+
+        for(int i=0;i<5;i++)
+            for(int j=0;j<5;j++)
+                if(matrixFields[i][j].isFocused()) {
+                    focusX=j;focusY=i;
+                }
+        System.out.println(focusY);
+
+                //UP button
+        if(imgbtns[1].isPressed() && focusY>0){
+            focusY--;
+            matrixFields[focusY][focusX].requestFocus();
+        }
+         else if(imgbtns[1].isPressed() && focusY==0 && focusX>0){
+            focusY=col;
+            focusX--;
+            matrixFields[focusY][focusX].requestFocus();
+        }
+
+        //Right button
+         if(imgbtns[2].isPressed() && focusX<row){
+            focusX++;
+            matrixFields[focusY][focusX].requestFocus();
+        }
+         else if(imgbtns[2].isPressed() && focusX==row && focusY<col){
+             focusY++;
+             focusX=0;
+             matrixFields[focusY][focusX].requestFocus();
+         }
+
+        //Left button
+         if((imgbtns[3].isPressed()  || imgbtns[0].isPressed()) && focusX>0){
+            focusX--;
+            matrixFields[focusY][focusX].requestFocus();
+        }
+         else if((imgbtns[3].isPressed()  || imgbtns[0].isPressed()) && focusX==0 && focusY>0){
+             focusY--;
+             focusX=row;
+             matrixFields[focusY][focusX].requestFocus();
+         }
+
+            //Down button
+         if(imgbtns[4].isPressed() && focusY<col){
+            focusY++;
+            matrixFields[focusY][focusX].requestFocus();
+        }
+         else if(imgbtns[4].isPressed() && focusY==col && focusX<row){
+             focusY=0;
+             focusX++;
+             matrixFields[focusY][focusX].requestFocus();
+         }
+
+    }
+
+    public void initializewidgets(){
+        editText=findViewById(R.id.text);
+        rowUp=findViewById(R.id.rowup);
+        rowDown=findViewById(R.id.rowdown);
+        colUp=findViewById(R.id.colup);
+        colDown=findViewById(R.id.coldown);
+
+
+
+
+        matrixFields[0][0]=findViewById(R.id.et00);
+        matrixFields[0][1]=findViewById(R.id.et01);
+        matrixFields[0][2]=findViewById(R.id.et02);
+        matrixFields[0][3]=findViewById(R.id.et03);
+        matrixFields[0][4]=findViewById(R.id.et04);
+
+
+        matrixFields[0][0].setShowSoftInputOnFocus(false);
+        matrixFields[0][1].setShowSoftInputOnFocus(false);
+        matrixFields[0][2].setShowSoftInputOnFocus(false);
+        matrixFields[0][3].setShowSoftInputOnFocus(false);
+        matrixFields[0][4].setShowSoftInputOnFocus(false);
+
+        matrixFields[1][0]=findViewById(R.id.et10);
+        matrixFields[1][1]=findViewById(R.id.et11);
+        matrixFields[1][2]=findViewById(R.id.et12);
+        matrixFields[1][3]=findViewById(R.id.et13);
+        matrixFields[1][4]=findViewById(R.id.et14);
+
+        matrixFields[1][0].setShowSoftInputOnFocus(false);
+        matrixFields[1][1].setShowSoftInputOnFocus(false);
+        matrixFields[1][2].setShowSoftInputOnFocus(false);
+        matrixFields[1][3].setShowSoftInputOnFocus(false);
+        matrixFields[1][4].setShowSoftInputOnFocus(false);
+
+        matrixFields[2][0]=findViewById(R.id.et20);
+        matrixFields[2][1]=findViewById(R.id.et21);
+        matrixFields[2][2]=findViewById(R.id.et22);
+        matrixFields[2][3]=findViewById(R.id.et23);
+        matrixFields[2][4]=findViewById(R.id.et24);
+
+        matrixFields[2][0].setShowSoftInputOnFocus(false);
+        matrixFields[2][1].setShowSoftInputOnFocus(false);
+        matrixFields[2][2].setShowSoftInputOnFocus(false);
+        matrixFields[2][3].setShowSoftInputOnFocus(false);
+        matrixFields[2][4].setShowSoftInputOnFocus(false);
+
+        matrixFields[3][0]=findViewById(R.id.et30);
+        matrixFields[3][1]=findViewById(R.id.et31);
+        matrixFields[3][2]=findViewById(R.id.et32);
+        matrixFields[3][3]=findViewById(R.id.et33);
+        matrixFields[3][4]=findViewById(R.id.et34);
+
+        matrixFields[3][0].setShowSoftInputOnFocus(false);
+        matrixFields[3][1].setShowSoftInputOnFocus(false);
+        matrixFields[3][2].setShowSoftInputOnFocus(false);
+        matrixFields[3][3].setShowSoftInputOnFocus(false);
+        matrixFields[3][4].setShowSoftInputOnFocus(false);
+
+        matrixFields[4][0]=findViewById(R.id.et40);
+        matrixFields[4][1]=findViewById(R.id.et41);
+        matrixFields[4][2]=findViewById(R.id.et42);
+        matrixFields[4][3]=findViewById(R.id.et43);
+        matrixFields[4][4]=findViewById(R.id.et44);
+
+        matrixFields[4][0].setShowSoftInputOnFocus(false);
+        matrixFields[4][1].setShowSoftInputOnFocus(false);
+        matrixFields[4][2].setShowSoftInputOnFocus(false);
+        matrixFields[4][3].setShowSoftInputOnFocus(false);
+        matrixFields[4][4].setShowSoftInputOnFocus(false);
+
+
+        //layouts
+        textInputLayouts[0][0]=findViewById(R.id.L00);
+        textInputLayouts[0][1]=findViewById(R.id.L01);
+        textInputLayouts[0][2]=findViewById(R.id.L02);
+        textInputLayouts[0][3]=findViewById(R.id.L03);
+        textInputLayouts[0][4]=findViewById(R.id.L04);
+
+        textInputLayouts[1][0]=findViewById(R.id.L10);
+        textInputLayouts[1][1]=findViewById(R.id.L11);
+        textInputLayouts[1][2]=findViewById(R.id.L12);
+        textInputLayouts[1][3]=findViewById(R.id.L13);
+        textInputLayouts[1][4]=findViewById(R.id.L14);
+
+        textInputLayouts[2][0]=findViewById(R.id.L20);
+        textInputLayouts[2][1]=findViewById(R.id.L21);
+        textInputLayouts[2][2]=findViewById(R.id.L22);
+        textInputLayouts[2][3]=findViewById(R.id.L23);
+        textInputLayouts[2][4]=findViewById(R.id.L24);
+
+        textInputLayouts[3][0]=findViewById(R.id.L30);
+        textInputLayouts[3][1]=findViewById(R.id.L31);
+        textInputLayouts[3][2]=findViewById(R.id.L32);
+        textInputLayouts[3][3]=findViewById(R.id.L33);
+        textInputLayouts[3][4]=findViewById(R.id.L34);
+
+        textInputLayouts[4][0]=findViewById(R.id.L40);
+        textInputLayouts[4][1]=findViewById(R.id.L41);
+        textInputLayouts[4][2]=findViewById(R.id.L42);
+        textInputLayouts[4][3]=findViewById(R.id.L43);
+        textInputLayouts[4][4]=findViewById(R.id.L44);
+
+
+        btns[0]=findViewById(R.id.btn0);
+        btns[1]=findViewById(R.id.btn1);
+        btns[2]=findViewById(R.id.btn2);
+        btns[3]=findViewById(R.id.btn3);
+        btns[4]=findViewById(R.id.btn4);
+        btns[5]=findViewById(R.id.btn5);
+        btns[6]=findViewById(R.id.btn6);
+        btns[7]=findViewById(R.id.btn7);
+        btns[8]=findViewById(R.id.btn8);
+        btns[9]=findViewById(R.id.btn9);
+        btns[10]=findViewById(R.id.btndot);
+
+        imgbtns[0]=findViewById(R.id.backspace);
+        imgbtns[1]=findViewById(R.id.up);
+        imgbtns[2]=findViewById(R.id.right);
+        imgbtns[3]=findViewById(R.id.left);
+        imgbtns[4]=findViewById(R.id.down);
+
+        rowText=findViewById(R.id.rowtext);
+        colText=findViewById(R.id.coltext);
+
+        ShowHideMatrix();
+
+    }
+
+    public void addCard(View v){
+
+        MainActivity.getInstance().inittextviews(index,editText.getText().toString());
+        if(act.getText().toString().length()==0){
+            til.setError("blah blah blah this line is for only showing red colored box");
+            Toast.makeText(this, "Matrix name field required", Toast.LENGTH_SHORT).show();
+        }
+
+        else
+        super.onBackPressed();
+    }
+
+    public void back(View v) {
+        super.onBackPressed();
+    }
+
+
+}
