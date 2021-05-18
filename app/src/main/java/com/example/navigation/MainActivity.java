f7e2737ea079105c011b0d9094b835af56b19c44
Ijlal Ahmad <ijlalahmad845@gmai.com>
2021-05-19 00:36:16 +0530

    Modified Dynamic EditTexts Functionality and Added Inputs to them, new method - KeyboardInput

--- app/src/main/java/com/example/navigation/MainActivity.java
+++ app/src/main/java/com/example/navigation/MainActivity.java
@@ -1,12 +1,9 @@
 package com.example.navigation;
 
-import androidx.annotation.IntegerRes;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.cardview.widget.CardView;
-import androidx.constraintlayout.solver.state.State;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.constraintlayout.widget.ConstraintSet;
 import androidx.core.view.GravityCompat;
@@ -14,42 +11,25 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
-import android.animation.TimeInterpolator;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.icu.text.RelativeDateTimeFormatter;
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.util.Log;
 import android.util.TypedValue;
-import android.view.LayoutInflater;
-import android.view.MenuItem;
-import android.view.MotionEvent;
+import android.view.Gravity;
 import android.view.View;
-import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.HorizontalScrollView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 
-import java.io.Serializable;
-import java.net.IDN;
 import java.util.ArrayList;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -63,6 +43,9 @@ public class MainActivity extends AppCompatActivity {
     HorizontalScrollView scrollView;
     ScrollView scrollView2;
     int counter=0;
+
+    Button[] buttons =new Button[12];
+    ImageButton[] imgbtns=new ImageButton[3];
 
 
     ArrayList<TextView> textlist=new ArrayList<>();
@@ -85,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
         verticalLL=findViewById(R.id.VerticalLL);
         scrollView2=findViewById(R.id.scrollView2);
 
-        initdrawer();
+        initializeWidgets();
 
     }
 
@@ -202,31 +185,72 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void addtextfield(View v){
+    public void addtextfield(){
         CardView cardView = new CardView(this);
         final float scale = cardView.getContext().getResources().getDisplayMetrics().density;
 
-        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+        LinearLayout.LayoutParams cardparams = new LinearLayout.LayoutParams(
                ViewGroup.LayoutParams.MATCH_PARENT,
                 (int) (160 * scale + 0.5f)
         );
-        params.setMargins((int) (10 * scale + 0.5f), (int) (20 * scale + 0.5f), (int) (10 * scale + 0.5f), (int) (0 * scale + 0.5f));
+        cardparams.setMargins((int) (10 * scale + 0.5f), (int) (20 * scale + 0.5f), (int) (10 * scale + 0.5f), (int) (0 * scale + 0.5f));
+
+        LinearLayout.LayoutParams editTextparams = new LinearLayout.LayoutParams(
+               ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        editTextparams.setMargins((int) (10 * scale + 0.5f), (int) (10 * scale + 0.5f), (int) (150 * scale + 0.5f), (int) (0 * scale + 0.5f));
 
         cardView.setElevation(20f);
-        cardView.setRadius(20f);
+        cardView.setRadius(10f);
+
 
         EditText editText=new EditText(this);
         editText.setShowSoftInputOnFocus(false);
+        editTextList.add(editText);
+       // editText.setText(String.valueOf(counter++));
 
-        cardView.addView(editText);
 
-        verticalLL.addView(cardView,verticalLL.getChildCount()-1,params);
+        cardView.addView(editText,editTextparams);
 
+        verticalLL.addView(cardView,verticalLL.getChildCount()-2,cardparams);
+        cardView.animate().alpha(1.0f).setDuration(200).setListener(null);
+       scrollView2.fullScroll(View.FOCUS_DOWN);
 
 
 
     }
-    public void initdrawer(){
+
+    public void KeyboardInput(View v){
+        String str;
+        int cursorindex;
+
+        for(int i=0;i<editTextList.size();i++) {
+            if (editTextList.get(i).isFocused()) {
+                str = editTextList.get(i).getText().toString();
+                cursorindex = editTextList.get(i).getSelectionStart();
+
+                for(int j=0;j<10;j++){
+                    if(buttons[j].isPressed()){
+                        editTextList.get(i).setText(str.substring(0,cursorindex)+j+str.substring(cursorindex));
+                        editTextList.get(i).setSelection(cursorindex+1);
+                    }
+                }
+
+                if(buttons[10].isPressed() && !editTextList.get(0).getText().toString().contains(".")){
+                    editTextList.get(i).setText(str.substring(0,cursorindex)+"."+str.substring(cursorindex));
+                    editTextList.get(i).setSelection(cursorindex+1);
+                }
+
+                else if (imgbtns[0].isPressed() && cursorindex>0){
+                    editTextList.get(i).setText(str.substring(0,cursorindex-1)+str.substring(cursorindex));
+                    editTextList.get(i).setSelection(cursorindex-1);
+                }
+
+            }
+        }
+    }
+    public void initializeWidgets(){
 
         Toolbar toolbar=findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -237,6 +261,60 @@ public class MainActivity extends AppCompatActivity {
         toggle=new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.open,R.string.close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
+
+        CardView cardView = new CardView(this);
+        final float scale = cardView.getContext().getResources().getDisplayMetrics().density;
+
+        LinearLayout.LayoutParams cardparams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                (int) (160 * scale + 0.5f)
+        );
+        cardparams.setMargins((int) (10 * scale + 0.5f), (int) (20 * scale + 0.5f), (int) (10 * scale + 0.5f), (int) (20 * scale + 0.5f));
+
+        /*LinearLayout.LayoutParams editTextparams = new LinearLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT
+        );
+        editTextparams.setMargins((int) (10 * scale + 0.5f), (int) (10 * scale + 0.5f), (int) (150 * scale + 0.5f), (int) (0 * scale + 0.5f));*/
+
+        cardView.setElevation(20f);
+        cardView.setRadius(10f);
+
+
+       TextView textView=new TextView(this);
+        textView.setGravity(Gravity.CENTER);
+        textView.setText("TAP TO ADD MORE CARDS");
+        textView.setTypeface(textView.getTypeface(), Typeface.BOLD);
+
+
+        cardView.addView(textView);
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addtextfield();
+            }
+        });
+
+
+       //cardView.animate().alpha(1.0f).setDuration(200).setListener(null);
+        //scrollView2.fullScroll(View.FOCUS_DOWN);
+
+                verticalLL.addView(cardView, verticalLL.getChildCount() - 1, cardparams);
+
+
+        buttons[0]=findViewById(R.id.button0);
+        buttons[1]=findViewById(R.id.button1);
+        buttons[2]=findViewById(R.id.button2);
+        buttons[3]=findViewById(R.id.button3);
+        buttons[4]=findViewById(R.id.button4);
+        buttons[5]=findViewById(R.id.button5);
+        buttons[6]=findViewById(R.id.button6);
+        buttons[7]=findViewById(R.id.button7);
+        buttons[8]=findViewById(R.id.button8);
+        buttons[9]=findViewById(R.id.button9);
+        buttons[10]=findViewById(R.id.buttonDot);
+
+        imgbtns[0]=findViewById(R.id.backspace);
 
     }
 
