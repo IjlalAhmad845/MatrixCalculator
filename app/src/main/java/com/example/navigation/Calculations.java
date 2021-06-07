0682a3146969f42d8673f1a93d05e2a4866c8980
Ijlal Ahmad <ijlalahmad845@gmai.com>
2021-06-07 08:27:53 +0530

    Added output display on Output cards of complex arithmetic expression without Matrices

--- app/src/main/java/com/example/navigation/Calculations.java
+++ app/src/main/java/com/example/navigation/Calculations.java
@@ -1,6 +1,9 @@
 package com.example.navigation;
 
 import android.util.Log;
+import android.view.View;
+
+import java.util.ArrayList;
 import java.util.Stack;
 
 public class Calculations {
@@ -10,7 +13,8 @@ public class Calculations {
 
     int outputCardIndex;
     String expression;
-    char[] cc=new char[60];
+    ArrayList<Character> ccList=new ArrayList<>();
+    ArrayList<Character> mmList=new ArrayList<>();
 
     Stack<Character> stringStack=new Stack<>();
 
@@ -19,8 +23,17 @@ public class Calculations {
     }
 
     public void setCc() {
-        for(int i=0;i<60;i++)
-            cc[i]=(char)(i+130);
+        for(int i=0;i<40;i++){
+           // System.out.println((char)(i+130));
+            ccList.add((char)(i+130));
+        }
+    }
+
+    public void setMm() {
+        for(int i=0;i<40;i++) {
+           // System.out.println((char) (i + 170));
+            mmList.add((char) (i + 170));
+        }
     }
 
     public void setOutputCardIndex(int outputCardIndex) {
@@ -28,7 +41,7 @@ public class Calculations {
     }
 
     public void masterControlFunction(){
-        setCc();
+        setCc();setMm();
         stringStack.clear();
         expression=instance.editTextList.get(outputCardIndex).getText().toString();
 
@@ -46,24 +59,47 @@ public class Calculations {
                     char c1=stringStack.pop();
                     char c2=stringStack.pop();
 
+                    if(!Character.isUpperCase(c1) && !Character.isUpperCase(c2))
                     switch (c){
                         case '+':
-                            EE.map.put(cc[i],EE.map.get(c2)+EE.map.get(c1));
-                            stringStack.push(cc[i]);
+                            EE.map.put(ccList.get(i),EE.map.get(c2)+EE.map.get(c1));
+                            stringStack.push(ccList.get(i));
                             break;
-                        case '-':EE.map.put(cc[i],EE.map.get(c2)-EE.map.get(c1));
-                            stringStack.push(cc[i]);
+                        case '-':EE.map.put(ccList.get(i),EE.map.get(c2)-EE.map.get(c1));
+                            stringStack.push(ccList.get(i));
                             break;
-                        case '·':EE.map.put(cc[i],EE.map.get(c2)*EE.map.get(c1));
-                            stringStack.push(cc[i]);
+                        case '·':EE.map.put(ccList.get(i),EE.map.get(c2)*EE.map.get(c1));
+                            stringStack.push(ccList.get(i));
                             break;
                     }
                 }
             }
-            Log.d("map", EE.map.get(stringStack.pop())+"");
-            }
-        else
-                instance.messageTextviewList.get(outputCardIndex).setText(EE.Convert(expression));
+            if(Character.isLowerCase(stringStack.elementAt(stringStack.size()-1)) || ccList.contains(stringStack.elementAt(stringStack.size()-1))){
+                Log.d("map", EE.map.get(stringStack.elementAt(stringStack.size()-1))+"");
+                for(int i=0;i<5;i++){
+                    for(int j=0;j<5;j++)
+                        instance.matrixOutputTextviewList.get(outputCardIndex).get(i).get(j).setVisibility(View.GONE);
+                }
+                instance.matrixOutputTextviewList.get(outputCardIndex).get(0).get(0).setVisibility(View.VISIBLE);
 
+                //typecasting to remove '.' in non decimal values
+                double result=EE.map.get(stringStack.pop());
+
+                result=Math.round(result*100.0)/100.0;
+
+                if(result!=(int)result)
+                instance.matrixOutputTextviewList.get(outputCardIndex).get(0).get(0).setText(result+"");
+                else
+                    instance.matrixOutputTextviewList.get(outputCardIndex).get(0).get(0).setText((int)result+"");
+            }
+
+            }
+        else if(expression.equals("")){
+            instance.messageTextviewList.get(outputCardIndex).setText("");
+            instance.matrixOutputTextviewList.get(outputCardIndex).get(0).get(0).setText("");
+        }
+
+        else
+            instance.messageTextviewList.get(outputCardIndex).setText(EE.Convert(expression));
     }
 }
