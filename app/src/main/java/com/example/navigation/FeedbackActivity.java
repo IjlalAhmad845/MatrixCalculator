6122fbc6d94ea3cff0ffdbd198f9ca44557719bb
Ijlal Ahmad <ijlalahmad845@gmail.com>
2021-07-28 12:59:44 +0530

    Designed layout of Feedback Activity with Telegram Link

--- app/src/main/java/com/example/navigation/FeedbackActivity.java
+++ app/src/main/java/com/example/navigation/FeedbackActivity.java
@@ -2,11 +2,16 @@ package com.example.navigation;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
+import android.widget.Button;
 
 public class FeedbackActivity extends AppCompatActivity {
 
+    Button joinButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,6 +27,16 @@ public class FeedbackActivity extends AppCompatActivity {
                 back();
             }
         });
+
+        findViewById(R.id.joinButton).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://t.me/joinchat/vL6zyE3Lig8yODVl"); // missing 'http://' will cause crashed
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+            }
+        });
+
     }
 
     private void back() {
